4757b529690777d8126e16b12756e59981e747cd
Hardy Joyson <hardyjoysan@gmail.com>
2021-01-26 14:03:35 +0530

    Vue build has been added as sailjs front-end layout

---
+++ server/api/controllers/ViewController.js
@@ -0,0 +1,9 @@
+module.exports = {
+    serve: (req, res) => {
+      const fs = require('fs')
+  
+      const app = __dirname + '/../../assets/index.html'
+      fs.createReadStream(app).pipe(res)
+    }
+  }
+  
